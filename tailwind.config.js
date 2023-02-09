689a3d85ea1ac43a0e5d866d22924dbc638ac706
alegonzalezcelis <alegonzalez1993@gmail.com>
2023-02-09 18:41:10 -0300

    Project created - Navbar FlowBite

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+              "./src/**/*.{html,js}",
+              "./node_modules/flowbite/**/*.js"
+            ],
+  theme: {
+    colors: {
+      'neutral': '#00171F',
+      'blue': '#003459',
+      'cyan': '#007EA7',
+      'sky': '#00A8E8'
+    },
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
